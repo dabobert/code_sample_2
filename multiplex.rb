ae661a51b118148fa8911a95843cbc94de26893f
Robert Jenkins <bobert@gmail.com>
2018-11-07 17:36:10 -0500

    progress in parsing showtimes, need to adjust some of the logic though

--- multiplex.rb
+++ multiplex.rb
@@ -1,10 +1,12 @@
-# debugging tools
+# gem: debugging tools
 require 'pry'
-# parse csv files
+# core: parse csv files
 require 'csv' 
-# loads  settings file
+# core: loads  settings file
 require 'yaml'
-# date/time arrithmetic
+# gem: handles rounding
+require 'rounding'
+# gem: date/time arithmetic
 require 'active_support'
 require 'active_support/core_ext/numeric/time'
 
@@ -35,8 +37,22 @@ class Multiplex
       row = Hash[orig_row.to_hash.map { |k, v| [k.to_s.strip.downcase.gsub(" ","_").to_sym, v.to_s.encode("utf-8", "binary", :undef => :replace).strip] }]
       info = row
 
-      run_time_hours, run_time_min= row[:run_time].split(":")
-      # total_time
+      run_time = Multiplex.convert_time_to_minutes_obj(row[:run_time])
+      total_time = run_time + @buffer_time
+
+      num_viewings = @hours_open.to_i/total_time
+
+      last = @close_time
+      showtimes = 0.upto(num_viewings).collect do |i|
+        # subtract running time
+        value = last - run_time
+        # round minutes to the latest 5 minute increment
+        rounded_minutes = value.strftime("%M").to_i.floor_to(5)
+        # set last to the computed value with the rounded minutes
+        last = value.change :min => rounded_minutes
+        [last, last + run_time]
+      end.reverse
+binding.pry
       @schedule << info
 
     end
@@ -55,10 +71,11 @@ class Multiplex
     end
     # doing all of this so we can use activesupport's date/time arithmetic functionality
     #   create open time object
-    @open_time  = @today + Multiplex.convert_time_to_minutes_obj(settings["#{key}_start".to_sym])
+    @open_time   = @today + Multiplex.convert_time_to_minutes_obj(settings["#{key}_start".to_sym])
     #   create close time object
-    @close_time = @today + Multiplex.convert_time_to_minutes_obj(settings["#{key}_end".to_sym])
+    @close_time  = @today + Multiplex.convert_time_to_minutes_obj(settings["#{key}_end".to_sym])
     @buffer_time = settings[:buffer_min].to_i.minutes
+    @hours_open  = @close_time - @open_time
   end
 
 
