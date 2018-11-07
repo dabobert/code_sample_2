916a46a9aa1f4bbf942f82966ca1647361f937bc
Robert Jenkins <bobert@gmail.com>
2018-11-07 16:16:18 -0500

    created Multiplex.convert_time_to_minutes_obj which converts a time in format hh:mm to ActiveSupport::Duration object

--- multiplex.rb
+++ multiplex.rb
@@ -14,24 +14,55 @@ require 'active_support/core_ext/numeric/time'
 
 class Multiplex
 
-  def initialize(file)
-    @file = file
-    # load the settings
-    YAML::load_file(File.join(__dir__, 'settings.yml'))
-    parse_movies
+  # converts a time in format hh:mm to ActiveSupport::Duration object
+  def self.convert_time_to_minutes_obj(string_time)
+    hours, min  = string_time.split(":")
+    hours.to_i.hours + min.to_i.minutes
   end
 
+  def initialize(file)
+    @file = file
+    @today = Time.now.beginning_of_day
+    @schedule = []
+    parse_settings
+    parse_movies
+    display_showtimes
+  end
 
   def parse_movies
+    puts @today.strftime("%A %m/%d/%Y")
     CSV.foreach(@file, :encoding=>"windows-1251:utf-8",:headers => true) do |orig_row|
       row = Hash[orig_row.to_hash.map { |k, v| [k.to_s.strip.downcase.gsub(" ","_").to_sym, v.to_s.encode("utf-8", "binary", :undef => :replace).strip] }]
-      theater = Theater.new(row)
+      info = row
+
+      run_time_hours, run_time_min= row[:run_time].split(":")
+      # total_time
+      @schedule << info
+
     end
   end
 
+  def parse_settings
+    # load the settings
+    settings = YAML::load_file(File.join(__dir__, 'settings.yml'))
+    # for testing purposes day of the week can be set via the settings
+    day_of_week = settings[:day_of_week] || @today.wday
+    # check if today is a weekday or not
+    if [1,2,3,4].include?(@today.wday)
+      key = "weekday"
+    else
+      key = "weekend"
+    end
+    # doing all of this so we can use activesupport's date/time arithmetic functionality
+    #   create open time object
+    @open_time  = @today + Multiplex.convert_time_to_minutes_obj(settings["#{key}_start".to_sym])
+    #   create close time object
+    @close_time = @today + Multiplex.convert_time_to_minutes_obj(settings["#{key}_end".to_sym])
+    @buffer_time = settings[:buffer_min].to_i.minutes
+  end
 
-  def theater(row)
 
+  def display_showtimes
   end
 
 end
