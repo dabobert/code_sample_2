b1860c9e23aa08c46986bc4acacd0ffdfda86e65
Robert Jenkins <bobert@gmail.com>
2018-11-10 11:50:37 -0500

    refactored spec, @today is now an instance variable

--- multiplex_spec.rb
+++ multiplex_spec.rb
@@ -15,14 +15,14 @@ describe Multiplex do
     before do
       test_settings_path = File.join(__dir__, 'test_settings.yml')
       test_data_path     = File.join(__dir__, 'test_data.csv')
+      @today             = Time.now.beginning_of_day
       @multiplex         = Multiplex.new(test_data_path, test_settings_path)
     end
 
     it "properly parsed the settings yml" do
-      today = Time.now.beginning_of_day
-      expect(@multiplex.open_time).to eq(today.change(:hour => 10, :min => 30))
-      expect(@multiplex.start_time).to eq(today.change(:hour => 11, :min => 30))
-      expect(@multiplex.close_time).to eq(today.change(:hour => 23, :min => 30))
+      expect(@multiplex.open_time).to eq(@today.change(:hour => 10, :min => 30))
+      expect(@multiplex.start_time).to eq(@today.change(:hour => 11, :min => 30))
+      expect(@multiplex.close_time).to eq(@today.change(:hour => 23, :min => 30))
       expect(@multiplex.cleanup_time).to eq(2100.seconds)
       expect(@multiplex.hours_open).to eq(43200.0)
     end
@@ -40,9 +40,4 @@ describe Multiplex do
     end
 
   end
-
-  # it "finds the factorial of 5" do
-  #   calculator = Factorial.new
-  #   expect(calculator.factorial_of(5)).to eq(120)
-  # end
 end
